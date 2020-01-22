1f47aad579fb4bcad7e6efd7b33e2a1bca10097d
Kubra Abbas <kubraabbas@QAIT.COM>
2020-01-22 22:28:21 +0530

    tatoc commit

---
+++ src/tatoc/Tatoc.java
@@ -0,0 +1,15 @@
+package tatoc;
+
+public class Tatoc {
+
+	public static void main(String[] args) {
+		Tatoclocators.launchTatoc();
+		Tatoclocators.gridGate();
+		Tatoclocators.frameDungeon();
+		Tatoclocators.dragAround();
+		Tatoclocators.popupWindows();
+		Tatoclocators.cookieHandling();
+	//	Tatoclocators.closeTatoc();
+	}
+}
+
